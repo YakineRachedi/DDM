65cb2a253c068ce50e35bb55e03e428b5db0c26b
Yakine <rachediyakine@gmail.com>
2026-07-23 15:19:48 +0200

    Add global mesh functions

--- mesh/mesh.py
+++ mesh/mesh.py
@@ -1,7 +1,47 @@
-from code import mesh, boundary
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+import numpy.linalg as la
 import numpy as np
+na = np.newaxis
+
+
+def mesh(nx,ny,Lx,Ly):
+   i = np.arange(0,nx)[na,:] * np.ones((ny,1), np.int64)
+   j = np.arange(0,ny)[:,na] * np.ones((1,nx), np.int64)
+   p = np.zeros((2,ny-1,nx-1,3), np.int64)
+   q = i+nx*j
+   p[:,:,:,0] = q[:-1,:-1]
+   p[0,:,:,1] = q[1: ,1: ]
+   p[0,:,:,2] = q[1: ,:-1]
+   p[1,:,:,1] = q[:-1,1: ]
+   p[1,:,:,2] = q[1: ,1: ]
+   v = np.concatenate(((Lx/(nx-1)*i)[:,:,na], (Ly/(ny-1)*j)[:,:,na]), axis=2)
+   vtx = np.reshape(v, (nx*ny,2))
+   elt = np.reshape(p, (2*(nx-1)*(ny-1),3))
+   return vtx, elt 
+
+def boundary(nx, ny):
+    bottom = np.hstack((np.arange(0,nx-1,1)[:,na],
+                        np.arange(1,nx,1)[:,na]))
+    top    = np.hstack((np.arange(nx*(ny-1),nx*ny-1,1)[:,na],
+                        np.arange(nx*(ny-1)+1,nx*ny,1)[:,na]))
+    left   = np.hstack((np.arange(0,nx*(ny-1),nx)[:,na],
+                        np.arange(nx,nx*ny,nx)[:,na]))
+    right  = np.hstack((np.arange(nx-1,nx*(ny-1),nx)[:,na],
+                        np.arange(2*nx-1,nx*ny,nx)[:,na]))
+    return np.vstack((bottom, top, left, right))
+
+def get_area(vtx, elt):
+    d = np.size(elt, 1)
+    if d == 2:
+        e = vtx[elt[:, 1], :] - vtx[elt[:, 0], :]
+        areas = la.norm(e, axis=1)
+    else:
+        e1 = vtx[elt[:, 1], :] - vtx[elt[:, 0], :]
+        e2 = vtx[elt[:, 2], :] - vtx[elt[:, 0], :]
+        areas = 0.5 * np.abs(e1[:,0] * e2[:,1] - e1[:,1] * e2[:,0])
+    return areas
+
 
 
 def local_mesh(Lx, Ly, Nx, Ny, j, J):
